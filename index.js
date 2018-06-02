0a6445cc656cb28a8c85661a4be42edc0fc9412e
Yarkhan <rasrpg@gmail.com>
2018-06-02 11:52:30 -0300

    created src directory
    it will hold our code from here on

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import { AppRegistry } from 'react-native'
+import App from './src/App'
 
-AppRegistry.registerComponent('drinkexplorer', () => App);
+AppRegistry.registerComponent('drinkexplorer', () => App)
