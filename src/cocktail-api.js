c944a0fe5cf66ca669bf4a2b56f742778f007d4f
Yarkhan <rasrpg@gmail.com>
2018-06-02 18:08:30 -0300

    Added caching for the cocktail-api
    CocktailDB is awfully slow. Added a cache for better experience.

--- src/cocktail-api.js
+++ src/cocktail-api.js
@@ -1,13 +1,37 @@
 /* global fetch */
-
+import { AsyncStorage } from 'react-native'
 const URL = 'https://www.thecocktaildb.com/api/json/v1/1/'
 
-const config = {
-  cache: 'force-cache'
+const parseJSON = async str => {
+  const parse = async str => JSON.parse(str)
+  return parse(str).catch(() => null)
 }
+
+const Cache = {
+  get: async key => AsyncStorage.getItem('drinkApiCache:' + key).then(parseJSON),
+  set: (key, value) => {
+    return AsyncStorage.setItem('drinkApiCache:' + key, JSON.stringify({
+      value,
+      age: Date.now()
+    }))
+  }
+}
+
 const _fetch = async uri => {
-  const json = await fetch(URL + uri, config).then(res => res.ok && res.json())
-  return json
+  const cache = await Cache.get(uri)
+  if (cache && cache.value && (cache.age + 1000 * 60 * 60) > Date.now()) return cache.value
+  const json = await fetch(URL + uri)
+    .then(async res => {
+      if (!res.ok) return null
+      const json = await res.json()
+      await Cache.set(uri, json)
+      return json
+    })
+    .catch(() => null)
+
+  if (json) return json
+  if (cache && cache.value) return cache.value
+  return null
 }
 
 export const categories = () => _fetch('list.php?c=list')
