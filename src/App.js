f0bf9aa7b5e469e17d8d625152f62eb05b18feb0
Yarkhan <rasrpg@gmail.com>
2018-06-02 23:36:19 -0300

    setup react-native-router-flux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
-import { Container, Content, Text, Button, Spinner, List, ListItem, Left, Right, Icon } from 'native-base'
-import AppHeader from './AppHeader'
 import Categories from './Categories/CategoriesContainer'
+import { Router, Stack, Scene } from 'react-native-router-flux'
+
 export default class App extends Component {
   render () {
     return (
-      <Container>
-        <AppHeader />
-        <Content>
-          <Categories />
-        </Content>
-      </Container>
+      <Router>
+        <Stack key='root' hideNavBar>
+          <Scene key='categories' component={Categories} />
+        </Stack>
+      </Router>
     )
   }
 }
--- src/Categories/Categories.js
+++ src/Categories/Categories.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { View } from 'react-native'
-import { Text, Button, List, ListItem, Left, Right, Icon, Spinner } from 'native-base'
+import { Text, Button, List, Container, Content, ListItem, Left, Right, Icon, Spinner } from 'native-base'
+import AppHeader from '../AppHeader'
 
 const Error = props => (
   <View>
@@ -18,13 +19,18 @@ export default props => {
   if (props.error) return <Error loadItems={props.loadItems} />
   if (!props.items.length) return <Spinner />
   return (
-    <List>
-      {props.items.map(item => (
-        <ListItem button onPress={() => console.log('clicked on', item)} key={item}>
-          <Left><Text>{item}</Text></Left>
-          <Right><Icon name='arrow-forward' /></Right>
-        </ListItem>
-      ))}
-    </List>
+    <Container>
+      <AppHeader />
+      <Content>
+        <List>
+          {props.items.map(item => (
+            <ListItem button onPress={() => console.log('clicked on', item)} key={item}>
+              <Left><Text>{item}</Text></Left>
+              <Right><Icon name='arrow-forward' /></Right>
+            </ListItem>
+          ))}
+        </List>
+      </Content>
+    </Container>
   )
 }
