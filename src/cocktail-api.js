036349af21a0bcfc053ff46da7acb7320acd9c22
Yarkhan <rasrpg@gmail.com>
2018-06-03 15:27:05 -0300

    update drink endpoint

--- src/cocktail-api.js
+++ src/cocktail-api.js
@@ -2,7 +2,7 @@
 import { AsyncStorage } from 'react-native'
 
 const URL = 'https://www.thecocktaildb.com/api/json/v1/1/'
-const MAX_CACHE_AGE = 15 * (1000 * 60)
+const MAX_CACHE_AGE = 60 * (1000 * 60)
 
 const parseJSON = async str => {
   const parse = async str => JSON.parse(str)
@@ -32,19 +32,50 @@ const _fetch = async uri => {
     .catch(() => null)
 
   if (json) return json
-  if (cache && cache.value) return cache.value
-  return null
+  // if (cache && cache.value) return cache.value
+  throw new Error('request error')
 }
 
 export const categories = () => _fetch('list.php?c=list')
   .then(({drinks}) => drinks.map(({strCategory}) => strCategory))
   .catch(() => null)
-export const category = name => _fetch(`list.php?c=${encodeURIComponent(name)}`)
+
+export const category = name => _fetch(`filter.php?c=${encodeURIComponent(name)}`)
   .then(({drinks}) => drinks.map(drink => ({
     id: drink.idDrink,
     name: drink.strDrink,
     thumbnail: drink.strDrinkThumb
   })))
+  .catch(() => null)
+
+const mapIngredients = drink => {
+  let ingredients = []
+  for (let i = 1; i <= 15; i++) {
+    const name = drink['strIngredient' + i]
+    const measure = drink['strMeasure' + i]
+    if (!name) break
+    const thumbnail = (
+      `https://www.thecocktaildb.com/images/ingredients/${encodeURIComponent(name)}-Small.png`
+    )
+    ingredients.push({name, measure, thumbnail})
+  }
+  return ingredients
+}
 
 export const drink = id => _fetch(`lookup.php?i=${id}`)
-  .then(({drinks}) => drinks[0])
+  .then(({drinks}) => {
+    const drink = drinks[0]
+    return ({
+      id: drink.idDrink,
+      name: drink.strDrink,
+      video: drink.strVideo,
+      category: drink.strCategory,
+      IBA: drink.strIBA,
+      alcoholic: drink.strAlcoholic,
+      glass: drink.strGlass,
+      instructions: drink.strInstructions,
+      thumbnail: drink.strDrinkThumb,
+      ingredients: mapIngredients(drink),
+    })
+  })
+  .catch(() => null)
